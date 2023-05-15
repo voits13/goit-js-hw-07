b3d800c8f367addbc8a509eb8338b86024ce170b
voits13 <110202532+voits13@users.noreply.github.com>
2023-05-16 02:08:22 +0300

    hw-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,62 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+const itemsMarkup = createGalleryImagesMarkup (galleryItems)
+let instance
+
+// Створюємо галерею картинок з масиву обєктів
+function createGalleryImagesMarkup (galleryItems) {
+return galleryItems.map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`;
+})
+.join('');
+}
+
+gallery.insertAdjacentHTML('beforeend', itemsMarkup)
+
+// Додаємо слухача події по кліку на картинку
+gallery.addEventListener('click', onGalleryImageClick)
+
+function onGalleryImageClick (evt) {
+evt.preventDefault();
+
+if (!evt.target.classList.contains('gallery__image')) {
+    return;
+}
+openModal(evt.target)
+}
+
+// Відкриття модального вікна з бібліотекою Lightbox
+function openModal (image) {
+instance = basicLightbox.create(
+  `<img src="${image.dataset.source}"/>`,
+  {
+    onShow: instance => {
+      window.addEventListener('keydown', closeByEsc);
+    },
+    onClose: instance => {
+      window.removeEventListener('keydown', closeByEsc);
+    },
+  }
+);
+instance.show();
+}
+
+// Закриття модального вікна ESC
+function closeByEsc ({code}) {
+    if (code === 'Escape') {
+        instance.close()
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", itemsMarkup);
+
+// Створюємо галерею картинок з масиву обєктів
+function createGalleryItemsMarkup(elem) {
+  return elem
+    .map(({ preview, original, description }) => {
+      return `<li>
+   <a class="gallery__item" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}" 
+        />
+   </a>
+ </li>`;
+    })
+    .join("");
+}
+
+// Підключаємо бібліотеку Lightbox
+// Ставимо підписи на картинки з затримкою показу
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
